5711a7d0071566a6afd503c02061c1a68216eb2e
Ryuichi Tokugami <magcot.com@gmail.com>
2016-08-11 19:41:22 +0900

    readInput added

--- src/main/java/com/tottokug/projectoxford/OxfordResponse.java
+++ src/main/java/com/tottokug/projectoxford/OxfordResponse.java
@@ -1,5 +1,10 @@
 package com.tottokug.projectoxford;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.apache.http.HttpResponse;
 
 public interface OxfordResponse<T extends OxfordResponse<?>> {
@@ -16,4 +21,13 @@ public interface OxfordResponse<T extends OxfordResponse<?>> {
 
 	<T extends OxfordResponse<?>> T build(HttpResponse response);
 
+	public static String readInput(InputStream is) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		StringBuffer json = new StringBuffer();
+		String line;
+		while ((line = br.readLine()) != null) {
+			json.append(line);
+		}
+		return json.toString();
+	}
 }
